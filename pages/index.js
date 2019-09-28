69e321b4daefd14748496ce8d8662de417e99742
Walter Lim <waltissomewhere@gmail.com>
2019-09-28 14:59:24 +1200

    fixed css crap

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Page from "../components/Page";
+import { Container, ContainerA } from "../components/components";
+
+
+
+const Index = () => (
+    <Page>
+        <Container>
+        <ContainerA>
+            <video></video>
+        </ContainerA>
+        <ContainerA></ContainerA>
+
+        </Container>
+    </Page>
+)
+export default Index
