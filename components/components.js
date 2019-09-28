a529b90635131e9fe5331d4b0a7a16538db5e293
Walter Lim <waltissomewhere@gmail.com>
2019-09-28 15:38:56 +1200

    first page done

--- components/components.js
+++ components/components.js
@@ -6,17 +6,42 @@ export const Container = styled.div`
 margin: 4rem auto;
 display: grid;
 grid-template-columns: 1fr 1fr;
-gap: 2rem;
+gap: 1rem;
 width: 60rem;
 height: 30rem;
 
 `;
 
 export const ContainerA = styled.div`
-background-color: pink;
+
 width: 100%;
 height: 100%;
 overflow: hidden;
+
+h1 {
+    margin-top: 6rem;
+    letter-spacing: -4px;
+    margin-bottom: 0;
+    font-size: 6rem;
+}
+
+p {
+    margin-top: 1rem;
+    font-size: 2rem;
+    line-height: 1.5;
+}
+
+button {
+    padding: 1rem;
+    font-size: 1.8rem;
+    font-family: 'Josefin Sans', sans-serif;
+    font-weight: bold;
+    border: 4px solid white;
+    border-radius: 6px;
+    background-color: white;
+    color: #9D00FF;
+}
+
 
 
 `
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,30 @@
-import Page from "../components/Page";
-import styled from "styled-components";
-import { Container, ContainerA } from "../components/components";
+import Page from '../components/Page'
+import styled from 'styled-components'
+import { Container, ContainerA } from '../components/components'
 
 const Image = styled.img`
- position: relative;
-  
-  height:100%;
+  position: relative;
+
+  height: 100%;
   overflow: hidden;
   object-fit: cover;
-  background-color: #9D00FF;
+  background-color: #9d00ff;
   z-index: 1;
-  `
+`
 
 const Index = () => (
-    <Page>
-        <Container>
-        <ContainerA>
-            <Image src='./static/welcome.png'/>
-
-        </ContainerA>
-        <ContainerA>aa</ContainerA>
-
-        </Container>
-    </Page>
+  <Page>
+    <Container>
+      <ContainerA>
+        <Image src='./static/welcome.png' />
+      </ContainerA>
+      <ContainerA>
+        <h1>Swearsies
+        </h1>
+        <p>Swearsies is a digital swear jar for your google home.</p>
+        <button>Get Started</button>
+      </ContainerA>
+    </Container>
+  </Page>
 )
 export default Index
